a41acdca7e13c5e3e787a10904a960ae7d964fa8
Deltonjr2 <Demetria.Farewell@yahoo.com>
2019-08-06 16:53:25 -0500

    Classwork

---
+++ Index.js
@@ -0,0 +1,12 @@
+alert("Hello World")
+
+
+var namevariable ="Demetria"
+alert ("Hello " +namevariable)
+
+
+var dogage ="5"
+alert (" Your dog is " + dogage * 7)
+
+alert ("1+1 = " + 1+1 )
+alert ("1+1 = " + (1+1) )
