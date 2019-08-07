f26bdca53919cae07d80343a914d531475578834
autumn-ragland <raglanda998@gmail.com>
2019-08-07 09:17:49 -0500

    Graded 08-07-2019 09:16:55

--- Index.js
+++ Index.js
@@ -10,3 +10,4 @@ alert (" Your dog is " + dogage * 7)
 
 alert ("1+1 = " + 1+1 )
 alert ("1+1 = " + (1+1) )
+// your second soltuion is the correct answer but you will recive partial credit for not commenting out you incorrect solution 
